d017101caab57dfb4e69ad064e361797572d7d0e
Henry Li <henry.li@okta.com>
2022-01-06 16:35:13 -0500

    refactor the extension for more features

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -4,7 +4,7 @@
  * ------------------------------------------------------------------------------------------ */
 
 import * as path from 'path';
-import { workspace, ExtensionContext } from 'vscode';
+import * as vscode from 'vscode';
 
 import {
 	LanguageClient,
@@ -18,63 +18,10 @@ import {
 	MarkupKind
 } from 'vscode-languageclient/node';
 
-export class WorkaroundFeature implements StaticFeature {
-	fillInitializeParams?: (params: InitializeParams) => void;
-	initialize(capabilities: ServerCapabilities<any>, documentSelector: DocumentSelector): void {
-		return;
-	}
-	dispose(): void {
-		return;
-	}
-    fillClientCapabilities(capabilities): void {
-        capabilities.textDocument.hover.contentFormat = [MarkupKind.Markdown];
-	}
-}
-
 let client: LanguageClient;
 
-export function activate(context: ExtensionContext) {
-	// The server is implemented in node
-	const serverModule = context.asAbsolutePath(
-		path.join('server', 'out', 'server.js')
-	);
-	// The debug options for the server
-	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-	const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
-
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
-	const serverOptions: ServerOptions = {
-		run: { module: serverModule, transport: TransportKind.ipc },
-		debug: {
-			module: serverModule,
-			transport: TransportKind.ipc,
-			options: debugOptions
-		}
-	};
-
-	// Options to control the language client
-	const clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'velocity' }],
-		synchronize: {
-			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-		}
-	};
-
-	// Create the language client and start the client.
-	client = new LanguageClient(
-		'languageServerExample',
-		'Language Server Example',
-		serverOptions,
-		clientOptions
-	);
-
-	client.registerFeature(new WorkaroundFeature());
-
-	// Start the client. This will also launch the server
-	client.start();
+export function activate(context: vscode.ExtensionContext) {
+	startLanguageClientAndServer(context);
 }
 
 export function deactivate(): Thenable<void> | undefined {
@@ -82,4 +29,61 @@ export function deactivate(): Thenable<void> | undefined {
 		return undefined;
 	}
 	return client.stop();
+}
+
+// export class WorkaroundFeature implements StaticFeature {
+// 	fillInitializeParams?: (params: InitializeParams) => void;
+// 	initialize(capabilities: ServerCapabilities<any>, documentSelector: DocumentSelector): void {
+// 		return;
+// 	}
+// 	dispose(): void {
+// 		return;
+// 	}
+//     fillClientCapabilities(capabilities): void {
+//         capabilities.textDocument.hover.contentFormat = [MarkupKind.Markdown];
+// 	}
+// }
+
+function startLanguageClientAndServer(context: vscode.ExtensionContext) {
+		// The server is implemented in node
+		const serverModule = context.asAbsolutePath(
+			path.join('server', 'out', 'server.js')
+		);
+		// The debug options for the server
+		// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+		const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+	
+		// If the extension is launched in debug mode then the debug server options are used
+		// Otherwise the run options are used
+		const serverOptions: ServerOptions = {
+			run: { module: serverModule, transport: TransportKind.ipc },
+			debug: {
+				module: serverModule,
+				transport: TransportKind.ipc,
+				options: debugOptions
+			}
+		};
+	
+		// Options to control the language client
+		const clientOptions: LanguageClientOptions = {
+			// Register the server for plain text documents
+			documentSelector: [{ scheme: 'file', language: 'velocity' }],
+			synchronize: {
+				// Notify the server about file changes to '.clientrc files contained in the workspace
+				fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc')
+			}
+		};
+	
+		// Create the language client and start the client.
+		client = new LanguageClient(
+			'languageServerExample',
+			'Language Server Example',
+			serverOptions,
+			clientOptions
+		);
+	
+		//client.registerFeature(new WorkaroundFeature());
+	
+		// Start the client. This will also launch the server
+		client.start();
 }
